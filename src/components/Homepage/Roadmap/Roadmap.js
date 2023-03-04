8e06b811c502ca2bcfcc4baa74f0f1107a879014
zohaibzyebi <zyebi553@gmail.com>
2023-03-04 16:22:05 +0500

    fix: animations in roadmap

--- src/components/Homepage/Roadmap/Roadmap.js
+++ src/components/Homepage/Roadmap/Roadmap.js
@@ -15,31 +15,32 @@ const Roadmap = () => {
         {/* Roadmap data  */}
         <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-2 md:gap-3 mdl:gap-5 lg:gap-12">
           {roadmap.data.map((item, index) => (
-            <Fade key={index} className="w-full" direction="up" triggerOnce>
-              <div className="w-full flex flex-col items-center justify-start gap-5 p-2 mdl:p-3 border-main border-solid border-2 bg-main bg-opacity-20">
-                <h2 className="text-main text-2xl font-bold ">{item.title}</h2>
-                <div className="w-full flex flex-col items-start justify-start gap-[7px]">
-                  {item.points.map((item, index) => (
-                    <Fade
-                      key={index}
-                      className="w-full"
-                      direction="up"
-                      triggerOnce
-                    >
-                      <div className="w-full flex items-start lg:items-center justify-start gap-1">
-                        <Dot
-                          color="#00A6CB"
-                          className="min-w-[20px] min-h-[20px] max-w-[20px] max-h-[20px]"
-                        />
-                        <p className="text-left text-white text-sm font-medium">
-                          {item}
-                        </p>
-                      </div>
-                    </Fade>
-                  ))}
-                </div>
+            <div
+              key={index}
+              className="w-full flex flex-col items-center justify-start gap-5 p-2 mdl:p-3 lg:py-7 border-main border-solid border-2 bg-main bg-opacity-20"
+            >
+              <h2 className="text-main text-2xl font-bold ">{item.title}</h2>
+              <div className="w-full flex flex-col items-start justify-start gap-[7px]">
+                {item.points.map((item, index) => (
+                  <Fade
+                    key={index}
+                    className="w-full"
+                    direction="up"
+                    triggerOnce
+                  >
+                    <div className="w-full flex items-start lg:items-center justify-start gap-1">
+                      <Dot
+                        color="#00A6CB"
+                        className="min-w-[20px] min-h-[20px] max-w-[20px] max-h-[20px]"
+                      />
+                      <p className="text-left text-white text-sm font-medium">
+                        {item}
+                      </p>
+                    </div>
+                  </Fade>
+                ))}
               </div>
-            </Fade>
+            </div>
           ))}
         </div>
       </div>
