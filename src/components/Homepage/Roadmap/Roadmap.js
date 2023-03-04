2f09d32f3b3007f8df29916f3a44d1fdcc72d0fc
zohaibzyebi <zyebi553@gmail.com>
2023-03-04 16:11:41 +0500

    done: roadmap section

--- src/components/Homepage/Roadmap/Roadmap.js
+++ src/components/Homepage/Roadmap/Roadmap.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { Fade } from "react-awesome-reveal";
+import { RxDotFilled as Dot } from "react-icons/rx";
 
 import Container from "@/components/Commons/Container/Container";
 import ResponsiveHeading from "@/components/Commons/ResponsiveHeading/ResponsiveHeading";
@@ -12,41 +13,28 @@ const Roadmap = () => {
       <div className="w-full  flex flex-col items-center justify-start gap-10 md:gap-12 lg:gap-16 py-10 md:py-12 lg:py-16">
         <ResponsiveHeading text="Roadmap" />
         {/* Roadmap data  */}
-        <div className="w-full flex items-center justify-start flex-col gap-6 sm:gap-10">
-          <Fade direction="up" triggerOnce>
-            <p className="w-full text-white text-base sm:text-lg text-center font-medium">
-              {roadmap.mainLabel}
-            </p>
-          </Fade>
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-2 md:gap-3 mdl:gap-5 lg:gap-12">
           {roadmap.data.map((item, index) => (
             <div
               key={index}
-              className="w-full max-w-[800px] flex flex-col items-center justify-start gap-2 sm:gap-3"
+              className="w-full flex flex-col items-center justify-start gap-5 p-2 mdl:p-3 border-main border-solid border-2 bg-main bg-opacity-20"
             >
-              <h2 className="w-full text-center text-xl text-main font-semibold  ">
-                {item.title}
-              </h2>
-              <div className="w-full flex flex-col items-center justify-start gap-1">
-                <Fade
-                  direction="up"
-                  cascade={0.25}
-                  triggerOnce
-                  className="w-full"
-                >
-                  {item.points.map((item, index) => (
-                    <div
-                      key={index}
-                      className="w-full border-2 border-main border-solid flex items-center justify-start gap-[10px] py-[7px] px-[10px] rounded sm:rounded-full bg-main bg-opacity-20"
-                    >
-                      <div className="w-[40px] h-[40px] flex items-center justify-center rounded sm:rounded-full border-solid border-main border-2  font-semibold text-lg leading-[0px] bg-main bg-opacity-40 text-white">
-                        {index + 1}
-                      </div>
-                      <p className="w-full text-left font-medium text-white">
-                        {item}
-                      </p>
-                    </div>
-                  ))}
-                </Fade>
+              <h2 className="text-main text-2xl font-bold ">{item.title}</h2>
+              <div className="w-full flex flex-col items-start justify-start gap-[7px]">
+                {item.points.map((item, index) => (
+                  <div
+                    key={index}
+                    className="w-full flex items-start lg:items-center justify-start gap-1"
+                  >
+                    <Dot
+                      color="#00A6CB"
+                      className="min-w-[20px] min-h-[20px] max-w-[20px] max-h-[20px]"
+                    />
+                    <p className="text-left text-white text-sm font-medium">
+                      {item}
+                    </p>
+                  </div>
+                ))}
               </div>
             </div>
           ))}
