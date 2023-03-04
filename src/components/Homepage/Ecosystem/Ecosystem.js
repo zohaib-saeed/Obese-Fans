8fed0c984126386fa1739e44bafb0987923abda2
zohaibzyebi <zyebi553@gmail.com>
2023-03-05 00:30:15 +0500

    fix: removed animation from ecosystem container

--- src/components/Homepage/Ecosystem/Ecosystem.js
+++ src/components/Homepage/Ecosystem/Ecosystem.js
@@ -12,35 +12,33 @@ const Ecosystem = () => {
     <Container id="ecosystem">
       <div className="w-full flex flex-col items-center justify-start gap-10 md:gap-12 lg:gap-16 py-10 md:py-12 lg:py-16">
         <ResponsiveHeading text="Ecosystem" />
-        <Fade direction="up" triggerOnce>
-          <div className="w-full grid grid-cols-1 sm:grid-cols-2 mdl:grid-cols-3  gap-3 mdl:gap-2 lg:gap-4">
-            {ecosystem.map((item, index) => (
-              <div
-                key={index}
-                className="w-full flex flex-col items-center justify-start gap-4 border-main border-solid border-2 p-4 py-7 rounded bg-main bg-opacity-20  lg:cursor-pointer lg:hover:scale-105 transition-all duration-300"
-              >
-                <div className="w-[130px] h-[130px] flex items-center justify-center relative ">
-                  <Image
-                    src={`/images/invest-in-us/${index + 1}.svg`}
-                    alt=""
-                    fill
-                    className="object-contain"
-                  />
-                </div>
-                <Fade direction="up" triggerOnce>
-                  <h4 className="text-2xl font-semibold text-main text-center w-full capitalize">
-                    {item.title}
-                  </h4>
-                </Fade>
-                <Fade direction="up" triggerOnce>
-                  <p className="text-white font-medium text-base w-full text-center">
-                    {item.description}
-                  </p>
-                </Fade>
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 mdl:grid-cols-3  gap-3 mdl:gap-2 lg:gap-4">
+          {ecosystem.map((item, index) => (
+            <div
+              key={index}
+              className="w-full flex flex-col items-center justify-start gap-4 border-main border-solid border-2 p-4 py-7 rounded bg-main bg-opacity-20  lg:cursor-pointer lg:hover:scale-105 transition-all duration-300"
+            >
+              <div className="w-[130px] h-[130px] flex items-center justify-center relative ">
+                <Image
+                  src={`/images/invest-in-us/${index + 1}.svg`}
+                  alt=""
+                  fill
+                  className="object-contain"
+                />
               </div>
-            ))}
-          </div>
-        </Fade>
+              <Fade direction="up" triggerOnce>
+                <h4 className="text-2xl font-semibold text-main text-center w-full capitalize">
+                  {item.title}
+                </h4>
+              </Fade>
+              <Fade direction="up" triggerOnce>
+                <p className="text-white font-medium text-base w-full text-center">
+                  {item.description}
+                </p>
+              </Fade>
+            </div>
+          ))}
+        </div>
       </div>
     </Container>
   );
